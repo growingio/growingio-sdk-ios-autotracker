0c77fbdecf389f72c4e90cd5516ffcd46578258a
BeyondChao <chengchaochao@growingio.com>
2020-08-05 13:59:26 +0800

    add example test

--- Growing/Example/GrowingIO_ExampleTests/GrowingIO_ExampleTests.m
+++ Growing/Example/GrowingIO_ExampleTests/GrowingIO_ExampleTests.m
@@ -7,6 +7,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <KIF/KIF.h>
+#import "MockEventQueue.h"
 
 @interface GrowingIO_ExampleTests : XCTestCase
 
@@ -14,8 +16,24 @@
 
 @implementation GrowingIO_ExampleTests
 
+
+// beforeEach 的作用参考KIF，简单概括就是构造测试Case的必要条件
+- (void)beforeEach {
+    [[viewTester usingLabel:@"chng请求"] tap];
+}
+
+// afterEach 的作用参考KIF，简单概括就是恢复app状态到初始状态，使得本case不对下次测试造成影响
+- (void)afterEach {
+    [[[[viewTester usingLabel:@"Previous"] usingTraits:UIAccessibilityTraitButton] usingAbsenceOfTraits:UIAccessibilityTraitKeyboardKey] tap];
+}
+
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
 
 - (void)tearDown {
@@ -23,15 +41,29 @@
 }
 
 - (void)testExample {
-    // This is an example of a functional test case.
+    // UI tests must launch the application that they test.
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+
+    // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
+//- (void)testExample {
+//    //构造chng事件
+//    [[viewTester usingLabel:@"UserName"] enterText:@"GrowingIOUser\n"];
+//    [[viewTester usingLabel:@"PassWord"] waitToBecomeFirstResponder];
+//    [[viewTester usingLabel:@"PassWord"] waitForView];
+//    [[viewTester usingLabel:@"PassWord"] enterText:@"GrowingIO" expectedResult:@"GrowingIO"];
+//
+//    //MockEventQueue 缓存事件并提供了几个高效的接口
+//    NSUInteger chngEventCount = [[MockEventQueue eventsFor:@"chng"] count];
+//
+//    NSUInteger acvEventCount = [[MockEventQueue eventsFor:@"app_activate"] count];
+//
+//    //使用XCTAssert断言，该部分可参考apple 文档
+//    XCTAssertEqual(chngEventCount, 1);
+//    XCTAssertEqual(acvEventCount, 1);
+//}
 
 @end
