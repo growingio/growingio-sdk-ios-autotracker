ce710b8b682f0570ea82b610dd75523ff823e2e0
sheng <277715243@qq.com>
2020-08-07 11:37:59 +0800

    feat: 工程整理，添加travis code check校验

---
+++ GrowingAnalytics.podspec
@@ -0,0 +1,37 @@
+#
+# Be sure to run `pod lib lint GrowingIO.podspec' to ensure this is a
+# valid spec before submitting.
+#
+# Any lines starting with a # are optional, but their use is encouraged
+# To learn more about a Podspec see https://guides.cocoapods.org/syntax/podspec.html
+#
+
+Pod::Spec.new do |s|
+  s.name             = 'GrowingAnalytics'
+  s.version          = '0.1.0'
+  s.summary          = 'A short description of GrowingIO.'
+  s.description      = <<-DESC
+TODO: Add long description of the pod here.
+                       DESC
+
+  s.homepage         = 'https://www.growingio.com/'
+  s.license          = { :type => 'Apache2.0', :file => 'LICENSE' }
+  s.author           = { 'GrowingIO' => 'support@growingio.com' }
+  s.source           = { :git => 'ssh://vcs-user@codes.growingio.com/diffusion/9/growingio-ios.git', :tag => s.version.to_s }
+  s.ios.deployment_target = '8.0'
+  s.requires_arc = true
+  s.static_framework = true
+  
+  s.subspec 'Tracker' do |tracker|
+      tracker.source_files = 'GrowingTracker/**/*'
+      tracker.public_header_files = 'GrowingTracker/Public/*.h'
+  end
+  
+  s.subspec 'AutoTracker' do |autotracker|
+      autotracker.source_files = 'GrowingAutoTracker/**/*'
+      autotracker.public_header_files = 'GrowingAutoTracker/Public/*.h'
+      autotracker.dependency 'GrowingAnalytics/Tracker'
+  end
+  
+
+end
---
+++ GrowingAutoTracker/AutoTrack/NSNotificationCenter+GrowingAutoTrack.m
@@ -0,0 +1,64 @@
+//
+//  NSNotificationCenter+GrowingAutoTrack.m
+//  GrowingAutoTracker
+//
+//  Created by GrowingIO on 2020/7/23.
+//  Copyright (C) 2020 Beijing Yishu Technology Co., Ltd.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+
+#import <UIKit/UIKit.h>
+#import "NSNotificationCenter+GrowingAutoTrack.h"
+#import "GrowingPropertyDefine.h"
+#import "GrowingClickEvent.h"
+#import "UIView+GrowingNode.h"
+
+
+
+GrowingPropertyDefine(UITextField, NSString *, growingHookOldText, setGrowingHookOldText)
+GrowingPropertyDefine(UITextView, NSString *, growingHookOldText, setGrowingHookOldText)
+
+@implementation NSNotificationCenter (GrowingAutoTrack)
+
+- (void)growing_postNotificationName:(NSNotificationName)aName object:(id)anObject userInfo:(NSDictionary *)aUserInfo {
+    
+    if ([aName isEqualToString:UITextFieldTextDidEndEditingNotification]) {
+        [self handleInputViewDidEndingEditing:anObject];
+    }
+    
+    [self growing_postNotificationName:aName object:anObject userInfo:aUserInfo];
+}
+
+- (void)handleInputViewDidEndingEditing:(id)anObject {
+    if ([anObject isKindOfClass:UITextField.class]) {
+        UITextField *inputView = (UITextField *)anObject;
+        NSString *text = inputView.text;
+        if (![inputView.growingHookOldText isEqualToString:text]) {
+            
+            inputView.growingHookOldText = text;
+            [GrowingTextEditContentChangeEvent sendEventWithNode:inputView andEventType:GrowingEventTypeUIChangeText];
+        }
+    }
+    
+    if ([anObject isKindOfClass:UITextView.class]) {
+        UITextView *inputView = (UITextView *)anObject;
+        NSString *text = inputView.text;
+        if (![inputView.growingHookOldText isEqualToString:text]) {
+            inputView.growingHookOldText = text;
+            [GrowingTextEditContentChangeEvent sendEventWithNode:inputView andEventType:GrowingEventTypeUIChangeText];
+        }
+    }
+}
+
+@end
