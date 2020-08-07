b6100a6c07a794470a5ad455dc635e663fa3a78e
sheng <277715243@qq.com>
2020-08-07 17:28:53 +0800

    ci: cetification验证

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -18,4 +18,4 @@ security find-identity -p codesigning  ~/Library/Keychains/ios-build.keychain
 echo " ****** "
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp "./scripts/profile/sdk_demo.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
+cp "./certificates/sdk_demo.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
