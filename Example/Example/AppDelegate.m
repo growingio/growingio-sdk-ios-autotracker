871acb1c8154360b78e194626c8ad0f8f310b2d6
sheng <277715243@qq.com>
2020-12-17 14:42:03 +0800

    fix: 修复设定location时，VISIT事件缺失位置信息bug

--- Example/Example/AppDelegate.m
+++ Example/Example/AppDelegate.m
@@ -35,7 +35,7 @@ static NSString *const kGrowingProjectId = @"0a1b4118dd954ec3bcc69da5138bdb96";
     configuration.impressionScale = 1.0;
     [GrowingAutotracker startWithConfiguration:configuration launchOptions:launchOptions];
 //    [GrowingTracker startWithConfiguration:configuration launchOptions:launchOptions];
-
+    [[GrowingAutotracker sharedInstance] setLocation:[@30.11 doubleValue] longitude:[@32.22 doubleValue]];
     [self registerRemoteNotification];
 
     return YES;
