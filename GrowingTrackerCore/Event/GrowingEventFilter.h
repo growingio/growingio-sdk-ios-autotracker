3d0e78622fdff10870be9ddf5d8d1b993a400cbf
rq <yanruixue@growingio.com>
2021-07-07 15:33:52 +0800

    fix: 优化代码格式

--- GrowingTrackerCore/Event/GrowingEventFilter.h
+++ GrowingTrackerCore/Event/GrowingEventFilter.h
@@ -21,53 +21,18 @@
 #import <Foundation/Foundation.h>
 
 typedef NS_OPTIONS(NSUInteger, GrowingFilterEventType) {
-    /**
-     *  0...000000000001 visit
-     */
+    
     GROWING_VISIT                     = (1 << 0),
-    /**
-     *  0...000000000010 custom
-     */
     GROWING_CUSTOM                    = (1 << 1),
-    /**
-     *  0...000000000100 visitor attributes
-     */
     GROWING_VISITOR_ATTRIBUTES        = (1 << 2),
-    /**
-     *  0...000000001000 login user attributes
-     */
     GROWING_LOGIN_USER_ATTRIBUTES     = (1 << 3),
-    /**
-     *  0...000000010000 conversion variables
-     */
     GROWING_CONVERSION_VARIABLES      = (1 << 4),
-    /**
-     *  0...000000100000 app closed
-     */
     GROWING_APP_CLOSED                = (1 << 5),
-    /**
-     *  0...000001000000 page
-     */
     GROWING_PAGE                      = (1 << 6),
-    /**
-     *  0...000010000000 page attributes
-     */
     GROWING_PAGE_ATTRIBUTES           = (1 << 7),
-    /**
-     *  0...000100000000 view click
-     */
     GROWING_VIEW_CLICK                = (1 << 8),
-    /**
-     *  0...001000000000 view change
-     */
     GROWING_VIEW_CHANGE               = (1 << 9),
-    /**
-     *  0...010000000000 form submit
-     */
     GROWING_FORM_SUBMIT               = (1 << 10),
-    /**
-     *  0...100000000000 reengage
-     */
     GROWING_REENGAGE                  = (1 << 11),
 };
 
