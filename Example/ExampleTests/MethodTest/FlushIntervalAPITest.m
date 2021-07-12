19687f3353fd998efd18c1f1ff664af2017e1bbe
lvyuqiang <lvyuqiang@growingio.com>
2021-07-12 16:25:06 +0800

    test: testcases

--- Example/ExampleTests/MethodTest/FlushIntervalAPITest.m
+++ Example/ExampleTests/MethodTest/FlushIntervalAPITest.m
@@ -14,10 +14,17 @@
 @implementation FlushIntervalAPITest
 
 -(void)test1GetFlushInterVal{
-
+    GrowingAutotrackConfiguration *configuration = [GrowingAutotrackConfiguration configurationWithProjectId:@"testProjectId"];
+    NSLog(@"流量上传间隔 %lu", (unsigned long)configuration.dataUploadInterval);
+    XCTAssertEqual((unsigned long)configuration.dataUploadInterval, 15);
+    [GrowingAutotracker startWithConfiguration:configuration launchOptions:nil];
 }
 
 -(void)test2SetFlushInterVal{
-   
+//    GrowingAutotrackConfiguration *configuration = [GrowingAutotrackConfiguration configurationWithProjectId:@"testProjectId2"];
+//    configuration.dataUploadInterval =10;
+//    NSLog(@"流量上传间隔 %lu", (unsigned long)configuration.dataUploadInterval);
+//    XCTAssertEqual((unsigned long)configuration.dataUploadInterval, 10);
+//    [GrowingAutotracker startWithConfiguration:configuration launchOptions:nil];
 }
 @end
