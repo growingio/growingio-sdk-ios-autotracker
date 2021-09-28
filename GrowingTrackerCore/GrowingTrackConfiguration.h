d84bf57d43fb2b3849f6436efbcfed9111dc14c5
YoloMao <a83506677@126.com>
2021-09-28 10:25:13 +0800

    feat: 新增 Id Mapping 开关

--- GrowingTrackerCore/GrowingTrackConfiguration.h
+++ GrowingTrackerCore/GrowingTrackConfiguration.h
@@ -25,16 +25,17 @@ FOUNDATION_EXPORT NSString * const kGrowingDefaultDataCollectionServerHost;
 
 @interface GrowingTrackConfiguration : NSObject <NSCopying>
 
-@property(nonatomic, copy, readonly) NSString *projectId;
-@property(nonatomic, assign) BOOL debugEnabled;
-@property(nonatomic, assign) NSUInteger cellularDataLimit;
-@property(nonatomic, assign) NSTimeInterval dataUploadInterval;
-@property(nonatomic, assign) NSTimeInterval sessionInterval;
-@property(nonatomic, assign) BOOL dataCollectionEnabled;
-@property(nonatomic, assign) BOOL uploadExceptionEnable;
-@property(nonatomic, copy) NSString *dataCollectionServerHost;
-@property(nonatomic, assign) NSUInteger excludeEvent;
-@property(nonatomic, assign) NSUInteger ignoreField;
+@property (nonatomic, copy, readonly) NSString *projectId;
+@property (nonatomic, assign) BOOL debugEnabled;
+@property (nonatomic, assign) NSUInteger cellularDataLimit;
+@property (nonatomic, assign) NSTimeInterval dataUploadInterval;
+@property (nonatomic, assign) NSTimeInterval sessionInterval;
+@property (nonatomic, assign) BOOL dataCollectionEnabled;
+@property (nonatomic, assign) BOOL uploadExceptionEnable;
+@property (nonatomic, copy) NSString *dataCollectionServerHost;
+@property (nonatomic, assign) NSUInteger excludeEvent;
+@property (nonatomic, assign) NSUInteger ignoreField;
+@property (nonatomic, assign) BOOL idMappingEnabled;
 
 - (instancetype)initWithProjectId:(NSString *)projectId;
 
