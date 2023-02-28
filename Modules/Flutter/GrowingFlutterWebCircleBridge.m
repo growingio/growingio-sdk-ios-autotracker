4b48868f65e4e4b73fec23e965036117c39712db
YoloMao <a83506677@126.com>
2023-02-28 14:15:26 +0800

    fix: 添加中间层 flutterWebCircleBridge，移除 __has_include 文件判断方式，避免 CI 失败

---
+++ Modules/Flutter/GrowingFlutterWebCircleBridge.m
@@ -0,0 +1,46 @@
+//
+//  GrowingFlutterWebCircleBridge.m
+//  GrowingAnalytics
+//
+//  Created by YoloMao on 2023/2/28.
+//  Copyright (C) 2022 Beijing Yishu Technology Co., Ltd.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#import "Modules/Flutter/GrowingFlutterWebCircleBridge.h"
+#import "Modules/Flutter/GrowingFlutterPlugin.h"
+
+GrowingService(GrowingFlutterService, GrowingFlutterWebCircleBridge)
+
+@implementation GrowingFlutterWebCircleBridge
+
++ (void)onFlutterCircleDataChange:(void(^_Nullable)(NSDictionary *_Nonnull data))handler {
+    GrowingFlutterPlugin *plugin = [GrowingFlutterPlugin sharedInstance];
+    plugin.onFlutterCircleDataChange = handler;
+}
+
++ (void)onWebCircleStart {
+    GrowingFlutterPlugin *plugin = [GrowingFlutterPlugin sharedInstance];
+    if (plugin.onWebCircleStart) {
+        plugin.onWebCircleStart();
+    }
+}
+
++ (void)onWebCircleStop {
+    GrowingFlutterPlugin *plugin = [GrowingFlutterPlugin sharedInstance];
+    if (plugin.onWebCircleStop) {
+        plugin.onWebCircleStop();
+    }
+}
+
+@end
