467137516464678ebdcec6839de8934ba3f6333e
YoloMao <a83506677@126.com>
2023-06-08 17:41:18 +0800

    test: update unit-testing

--- Example/GrowingAnalyticsTests/ModulesTests/WebCircleTests/WebCircleTest.m
+++ Example/GrowingAnalyticsTests/ModulesTests/WebCircleTests/WebCircleTest.m
@@ -27,7 +27,6 @@
 #import "GrowingTrackerCore/DeepLink/GrowingDeepLinkHandler.h"
 #import "GrowingTrackerCore/Event/GrowingNodeProtocol.h"
 #import "Modules/WebCircle/GrowingWebCircle.h"
-#import "GrowingAutotrackerCore/Page/GrowingPageGroup.h"
 #import "GrowingAutotrackerCore/Page/GrowingPageManager.h"
 #import "GrowingAutotrackerCore/Autotrack/UIViewController+GrowingAutotracker.h"
 #import "Modules/WebCircle/GrowingWebCircleElement.h"
@@ -103,7 +102,7 @@
 
     UIViewController *current = [[UIViewController alloc] init];
     // 避免自动发 PAGE 报错
-    GrowingPageGroup *page = [current growingPageObject];
+    GrowingPage *page = [current growingPageObject];
     if (!page) {
         [[GrowingPageManager sharedInstance] createdViewControllerPage:current];
         page = [current growingPageObject];
