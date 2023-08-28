3150be099cf7bfa80eac7c928d29655ac02426dc
YoloMao <a83506677@126.com>
2023-08-28 15:11:58 +0800

    fix: rename modules apis by using NS_SWIFT_NAME

--- Modules/Hybrid/Public/GrowingHybridModule.h
+++ Modules/Hybrid/Public/GrowingHybridModule.h
@@ -24,19 +24,22 @@ NS_ASSUME_NONNULL_BEGIN
 
 @class WKWebView;
 
+NS_SWIFT_NAME(Hybrid)
 @interface GrowingHybridModule : NSObject <GrowingModuleProtocol>
+
++ (instancetype)sharedInstance;
 
 // 是否对所有webView自动注入Hybrid SDK，默认为YES
 @property (nonatomic, assign) BOOL autoBridgeEnabled;
 
 // 在autoBridgeEnabled为NO时，对单个webView启用Hybrid注入，请在主线程调用
-- (void)enableBridgeForWebView:(WKWebView *)webView;
+- (void)enableBridgeForWebView:(WKWebView *)webView NS_SWIFT_NAME(enableBridge(_:));
 
 // 在autoBridgeEnabled为YES时，对单个webView关闭Hybrid注入，请在主线程调用
-- (void)disableBridgeForWebView:(WKWebView *)webView;
+- (void)disableBridgeForWebView:(WKWebView *)webView NS_SWIFT_NAME(disableBridge(_:));
 
 // 判断当前配置下，webView是否可注入
-- (BOOL)isBridgeForWebViewEnabled:(WKWebView *)webView;
+- (BOOL)isBridgeForWebViewEnabled:(WKWebView *)webView NS_SWIFT_NAME(isBridgeEnabled(_:));
 
 // 重置Hybrid注入配置，请在主线程调用
 - (void)resetBridgeSettings;
