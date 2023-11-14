6548bd9211ecf71248eff17aab07df1ed4e52a3b
YoloMao <a83506677@126.com>
2023-11-14 17:39:22 +0800

    fix: remove codes about md5 (weak hash, not safe enough)

--- GrowingTrackerCore/Helpers/Foundation/NSData+GrowingHelper.m
+++ GrowingTrackerCore/Helpers/Foundation/NSData+GrowingHelper.m
@@ -120,33 +120,6 @@
     }
 }
 
-- (void)growingHelper_md5value:(unsigned char *)valueArray {
-    CC_MD5(self.bytes, (CC_LONG)[self length], valueArray);
-}
-
-- (NSString *)growingHelper_md5String {
-    unsigned char result[16];
-    [self growingHelper_md5value:result];
-    NSString *retVal = [NSString stringWithFormat:@"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
-                                                  result[0],
-                                                  result[1],
-                                                  result[2],
-                                                  result[3],
-                                                  result[4],
-                                                  result[5],
-                                                  result[6],
-                                                  result[7],
-                                                  result[8],
-                                                  result[9],
-                                                  result[10],
-                                                  result[11],
-                                                  result[12],
-                                                  result[13],
-                                                  result[14],
-                                                  result[15]];
-    return retVal;
-}
-
 - (NSData *)growingHelper_xorEncryptWithHint:(unsigned char)hint {
     id<GrowingEncryptionService> service =
         [[GrowingServiceManager sharedInstance] createService:@protocol(GrowingEncryptionService)];
