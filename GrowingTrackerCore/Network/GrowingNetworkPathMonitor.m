fd2227a7f1ad2835f6b7fe84bbaeb2fc61b14971
GIOSDK <sdk-integration@growingio.com>
2024-02-06 07:32:36 +0000

    style: code format

--- GrowingTrackerCore/Network/GrowingNetworkPathMonitor.m
+++ GrowingTrackerCore/Network/GrowingNetworkPathMonitor.m
@@ -86,7 +86,8 @@
 #if Growing_OS_VISION
 - (GrowingNetworkReachabilityStatus)reachabilityStatusForPath:(nw_path_t)path {
 #else
-- (GrowingNetworkReachabilityStatus)reachabilityStatusForPath:(nw_path_t)path API_AVAILABLE(ios(12.0), tvos(12.0), macos(10.14), watchos(6.0)) {
+- (GrowingNetworkReachabilityStatus)reachabilityStatusForPath:(nw_path_t)path
+    API_AVAILABLE(ios(12.0), tvos(12.0), macos(10.14), watchos(6.0)) {
 #endif
     nw_path_status_t status = nw_path_get_status(path);
     if (status != nw_path_status_satisfied) {
